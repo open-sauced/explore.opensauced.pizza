0a915b640b717409b7e6c2a3249112f37b9b9bd5
Matthew <mtfoley.mae@gmail.com>
2021-07-23 22:08:06 -0400

    fix queries and re-indent

--- src/Queries.js
+++ src/Queries.js
@@ -393,181 +393,181 @@ query FetchGoal($number: Int!) {
 }        
     `,
     FetchGoals:`query FetchGoals() {
-    gitHub {
-        viewer {
-        repository(name: "open-sauced-goals") {
-            id
-            data: object(expression: "HEAD:data.json") {
-            id
-            ... on GitHubBlob {
-                id
-                text
-            }
-            }
-            issues(
-            first: 50
-            states: OPEN
-            orderBy: { direction: DESC, field: CREATED_AT }
-            ) {
-            totalCount
-            nodes {
-                id
-                full_name: title
-                body
-                number
-                labels(first: 3) {
-                nodes {
-                    color
-                    name
-                    id
-                }
-                }
-            }
-            }
-        }
-        }
-    }
-}
-`,
-    FetchOwnerQuery:`query FetchOwnerQuery($owner: String!) {
-        gitHub {
-          user(login: $owner) {
-            id
-          }
-        }
-      }`,
-    FetchMemberStatusQuery:`query FetchMemberStatusQuery() {
-        gitHub {
-          viewer {
-            organization(login: "open-sauced") {
-              viewerIsAMember
-            }
-          }
-        }
-      }`,
-      FetchRateLimitQuery:`query FetchRateLimitQuery() {
-        gitHub {
-          rateLimit {
-            remaining
-          }
-        }
-      }`,
-      FetchRepoCountQuery:`query FetchRepoCountQuery() {
-        gitHub {
-          search(query: "open-sauced-goals", type: REPOSITORY) {
-            repositoryCount
-          }
-        }
-      }`,
-      CreateOpenSaucedGoalsRepo:`mutation CreateOpenSaucedGoalsRepo($ownerId: ID!) {
-        gitHub {
-          cloneTemplateRepository(
-            input: {
-              repositoryId: "MDEwOlJlcG9zaXRvcnkyNjYzNDYyNDM="
-              visibility: PUBLIC
-              ownerId: $ownerId
-              name: "open-sauced-goals"
-            }
-          ) {
-            repository {
+  gitHub {
+    viewer {
+      repository(name: "open-sauced-goals") {
+        id
+        data: object(expression: "HEAD:data.json") {
+          id
+          ... on GitHubBlob {
               id
-              name
-              nameWithOwner
-              url
-              issues(
-                first: 10
-                states: OPEN
-                orderBy: { direction: DESC, field: CREATED_AT }
-              ) {
-                totalCount
-                nodes {
-                  id
-                  title
-                  body
-                  number
-                  labels(first: 3) {
-                    nodes {
-                      color
-                      name
-                      id
-                    }
-                  }
-                }
+              text
+          }
+        }
+        issues(
+          first: 50
+          states: OPEN
+          orderBy: { direction: DESC, field: CREATED_AT }
+        ) {
+          totalCount
+          nodes {
+            id
+            full_name: title
+            body
+            number
+            labels(first: 3) {
+            nodes {
+                color
+                name
+                id
+            }
+          }
+        }
+      }
+    }
+  }
+  }
+}`,
+    FetchOwnerQuery:`query FetchOwnerQuery($owner: String!) {
+  gitHub {
+    user(login: $owner) {
+      id
+    }
+  }
+}`,
+    FetchMemberStatusQuery:`query FetchMemberStatusQuery() {
+  gitHub {
+    viewer {
+      organization(login: "open-sauced") {
+        viewerIsAMember
+      }
+    }
+  }
+}`,
+      FetchRateLimitQuery:`query FetchRateLimitQuery() {
+  gitHub {
+    rateLimit {
+      remaining
+    }
+  }
+}`,
+      FetchRepoCountQuery:`query FetchRepoCountQuery() {
+  gitHub {
+    search(query: "open-sauced-goals", type: REPOSITORY) {
+      repositoryCount
+    }
+  }
+}`,
+      CreateOpenSaucedGoalsRepo:`mutation CreateOpenSaucedGoalsRepo($ownerId: ID!) {
+  gitHub {
+    cloneTemplateRepository(
+      input: {
+        repositoryId: "MDEwOlJlcG9zaXRvcnkyNjYzNDYyNDM="
+        visibility: PUBLIC
+        ownerId: $ownerId
+        name: "open-sauced-goals"
+      }
+    ) {
+      repository {
+        id
+        name
+        nameWithOwner
+        url
+        issues(
+          first: 10
+          states: OPEN
+          orderBy: { direction: DESC, field: CREATED_AT }
+        ) {
+          totalCount
+          nodes {
+            id
+            title
+            body
+            number
+            labels(first: 3) {
+              nodes {
+                color
+                name
+                id
               }
             }
           }
         }
-      }`,
+      }
+    }
+  }
+}`,
     CreateGoal:`mutation CreateGoal(
-        $repoId: ID!
-        $title: String!
-        $notes: String
-      ) {
-        __typename
-        gitHub {
-          createIssue(
-            input: {
-              title: $title
-              repositoryId: $repoId
-              body: $notes
-            }
-          ) {
-            issue {
-              id
-              title
-            }
-          }
-        }
-      }`,
+  $repoId: ID!
+  $title: String!
+  $notes: String
+) {
+  __typename
+  gitHub {
+    createIssue(
+      input: {
+        title: $title
+        repositoryId: $repoId
+        body: $notes
+      }
+    ) {
+      issue {
+        id
+        title
+      }
+    }
+  }
+}`,
     UpdateGoal:`mutation UpdateGoal(
-        $id: ID!
-        $state: GitHubIssueState
-        $title: String
-        $notes: String
-      ) {
-        __typename
-        gitHub {
-          updateIssue(
-            input: {
-              id: $id
-              state: $state
-              title: $title
-              body: $notes
-            }
-          ) {
-            issue {
-              id
-              body
-            }
-          }
-        }
-      }`,
+  $id: ID!
+  $state: GitHubIssueState
+  $title: String
+  $notes: String
+) {
+  __typename
+  gitHub {
+    updateIssue(
+      input: {
+        id: $id
+        state: $state
+        title: $title
+        body: $notes
+      }
+    ) {
+      issue {
+        id
+        body
+      }
+    }
+  }
+}`,
     FetchUserForkCount:`query FetchUserForkCount(
-        $repoName: String!
-        $repoOwner: String!
-      ) {
-        gitHub {
-          repository(name: $repoName, owner: $repoOwner) {
-            forks(affiliations: OWNER) {
-              totalCount
-            }
-          }
-        }`,
+  $repoName: String!
+  $repoOwner: String!
+) {
+  gitHub {
+    repository(name: $repoName, owner: $repoOwner) {
+      forks(affiliations: OWNER) {
+        totalCount
+      }
+    }
+  }
+}`,
     ForkRepository:`mutation ForkRepository(
-        $repoName: String!
-        $repoOwner: String!
-      ) {
-        gitHub {
-          createFork_oneGraph(
-            input: { repoName: $repoName, repoOwner: $repoOwner }
-          ) {
-            clientMutationId
-            repository {
-              id
-              url
-            }
-          }
-        }
-      }`
+  $repoName: String!
+  $repoOwner: String!
+) {
+  gitHub {
+    createFork_oneGraph(
+      input: { repoName: $repoName, repoOwner: $repoOwner }
+    ) {
+      clientMutationId
+      repository {
+        id
+        url
+      }
+    }
+  }
+}`
 };
 export default Queries;
